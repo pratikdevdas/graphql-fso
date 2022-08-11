60bb10215c14c8f4b1bb756bd6336d1c57c2fb83
Revenant <dasdev.pratik@gmail.com>
2022-08-11 22:03:43 +0530

    updatin number

--- index.js
+++ index.js
@@ -53,6 +53,10 @@ const typeDefs = gql`
       street: String!
       city: String!
     ): Person
+    editNumber(
+      name: String!
+      phone: String!
+    ):Person
   }
 `;
 
@@ -93,6 +97,16 @@ const resolvers = {
       persons = persons.concat(person);
       return person;
     },
+    editNumber: (root, args) => {
+      const person = persons.find(p => p.name === args.name)
+      if(!person){
+        return null
+      }
+
+      const updatedPerson = {...person, phone: args.phone}
+      persons = persons.map(p => p.name === args.name ? updatedPerson : p)
+      return updatedPerson
+    }
   },
 };
 
